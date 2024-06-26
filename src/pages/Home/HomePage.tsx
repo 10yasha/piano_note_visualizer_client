a74e4e4ebd7314503352c5f5d87e89f4c35d10fc
Justin Huang <justin.huang@fotechsolutions.com>
2024-06-26 17:09:41 -0600

    basic homepage

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,8 +1,28 @@
+import "./HomePage.css";
+
 export default function HomePage() {
   return (
     <div className="home-page">
-      <h1>Home</h1>
-      <p>Put welcome, recommendations, and search here</p>
+      <header className="header">
+        <div className="portraits">
+          <div className="portrait">
+            <img
+              src="/src/worship/dmitri_shostakovich.jpg"
+              alt="Dmitri Shostakovich"
+            />
+          </div>
+          <div className="portrait">
+            <img src="/src/worship/j_s_bach.jpg" alt="J S Bach" />
+          </div>
+          <div className="portrait">
+            <img src="/src/worship/chick_corea.jpg" alt="Chick Corea" />
+          </div>
+          <div className="portrait">
+            <img src="/src/worship/bill_evans.jpg" alt="Bill Evans" />
+          </div>
+        </div>
+        <h1>Piano Visualizer</h1>
+      </header>
     </div>
   );
 }
