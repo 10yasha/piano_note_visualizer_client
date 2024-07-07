d84f2daae5aad06e4cd590e507442defc855d272
Justin Huang <justin.huang@fotechsolutions.com>
2024-07-07 17:20:43 -0600

    start setting up manage page

--- src/pages/Manage/ManagePage.tsx
+++ src/pages/Manage/ManagePage.tsx
@@ -1,10 +1,30 @@
+import { Link } from "react-router-dom";
+
+import { useAllRecordings } from "../../hooks/useAllRecordings";
 import { addRecording } from "./ManageApiHelper";
 
+import "./ManagePage.css";
+
 export default function ManagePage() {
+  const records = useAllRecordings();
+
   return (
     <div className="manage-page">
-      <h1>Manage</h1>
-      <p>Crud here.</p>
+      <div className="create-recording-panel">
+        <p>create here</p>
+      </div>
+      <div className="edit-recordings-panel">
+        <div className="scroll-container">
+          {records &&
+            records.map((record) => (
+              <div className="row">
+                <Link key={record.id} to={`/browse/${record.url}`}>
+                  {record.name}
+                </Link>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
